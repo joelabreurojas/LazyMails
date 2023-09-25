733fe814d88a5be054ab2194ba168c8febcd87d3
Joel Abreu <joelabreurojas@gmail.com>
2023-09-24 20:01:40 -0400

    refactor: different interaction for chat_type

--- lazymails/queries/bot.py
+++ lazymails/queries/bot.py
@@ -92,10 +92,14 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     print(f'User {update.message.chat.id} in {message_type}: "{text}"')
 
-    if config('BOT_USERNAME') in text:
-        text: str = text.replace(config('BOT_USERNAME'), '').strip()
-
-    response: str = handle_response(text)
+    if message_type == 'group':
+        if config('BOT_USERNAME') in text:
+            new_text: str = text.replace(config('BOT_USERNAME'), '').strip()
+            response: str = handle_response(new_text)
+        else:
+            return
+    else:
+        response: str = handle_response(text)
 
     print('Bot', response)
     await update.message.reply_text(response)
